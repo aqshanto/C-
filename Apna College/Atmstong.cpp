3e80ac3182d5de17a0df80c8a79e349ede226930
aqShanto <abdulquymshanto@gmail.com>
2024-05-01 00:16:22 +0600

    Update

---
+++ Apna College/Atmstong.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int original=n;
+    double sum=0.0;
+    while(n!=0){
+        int remider=n%10;
+        sum+=pow(remider,3);
+        n/=10;
+    }
+    if(sum==original){
+        cout<<"ArmStrong Number"<<endl;
+    }else{
+        cout<<"NOT"<<endl;
+    }
+}
