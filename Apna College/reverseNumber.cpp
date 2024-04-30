433c0519fdf198c1a652dde0f074cecb8e056ae4
aqShanto <abdulquymshanto@gmail.com>
2024-05-01 02:06:29 +0600

    update

--- Apna College/reverseNumber.cpp
+++ Apna College/reverseNumber.cpp
@@ -6,10 +6,13 @@ int main(){
     cin>>n;
 
     int reverse=0;
+
     while(n!=0){
         int reminder=n%10;
         reverse=reverse*10+reminder;
         n/=10;
     }
-    cout<<reverse;
+
+    cout<<reverse<<endl;
+    return 0;
 }
