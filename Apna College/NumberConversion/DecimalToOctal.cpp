012fa5def5f77ef61565af0d70a45f4c0516d3a4
aqShanto <abdulquymshanto@gmail.com>
2024-05-01 17:30:04 +0600

    Number Conversion

--- Apna College/NumberConversion/DecimalToOctal.cpp
+++ Apna College/NumberConversion/DecimalToOctal.cpp
@@ -2,26 +2,25 @@
 
 using namespace std;
 
-int DecimalToBinary(int n){
+int DecimalToOctal(int n){
     int ans=0;
     int x=1;
     while(x<=n){
-        x*=2;
+        x*=8;
     }
-    x/=2;
-    while (x>0){
-        int lastDigit=n/x;
+    x/=8;
+    while(x>0){
+        int lastDigit=n/x; 
         n-=lastDigit*x;
-        x/=2;
+        x/=8;
         ans=ans*10+lastDigit;
     }
-    return ans;
 }
 
 int main(){
     int n;
     cin>>n;
 
-    cout<<DecimalToBinary(n)<<endl;
+    cout<<DecimalToOctal(n)<<endl;
     return 0;
 }
